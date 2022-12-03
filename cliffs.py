492d05d705f22d13aa0d48f92a7c4ca9d9532530
David Kellerman <david@silvermoon.org>
2022-12-03 14:36:29 -0500

    Q as np array

--- cliffs.py
+++ cliffs.py
@@ -15,8 +15,10 @@ import sys
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from pprint import pprint
 from collections import defaultdict
 from tqdm import tqdm
+
 
 # game config
 size = (10, 4)
@@ -24,6 +26,7 @@ width, height = size
 cliffs = tuple([(x, height - 1) for x in range(1, width - 1)])
 goal = (width - 1, height - 1)
 action_map = dict(up=(0, -1), down=(0, 1), left=(-1, 0), right=(1, 0))
+action_keys = list(action_map.keys())
 rewards = dict(cliff=-100, step=-1, goal=0)
 max_steps = 200
 verbose = False
@@ -33,13 +36,22 @@ agent = None
 history = None
 step_ct = None
 random_ct = None
-Q = defaultdict(lambda: 0)
 
 # hyperparameters
 learning_rate = .5
 epsilon = .1
 discount = 1.0
 use_sarsa = True
+
+Q = np.zeros((width, height, len(action_map)))
+
+
+def get_qval(state, action):
+    return Q[(*state, action_keys.index(action))]
+
+
+def set_qval(state, action, val):
+    Q[(*state, action_keys.index(action))] = val
 
 
 def debug(*args):
@@ -113,7 +125,7 @@ def get_action_space():
 def get_policy_action(state, use_epsilon=True, lookahead=False):
     global random_ct
     actions = get_action_space()
-    scores = [Q[(state, a)] for a in actions]
+    scores = [get_qval(state, a) for a in actions]
     if use_epsilon and (random.random() < epsilon):
         idx = random.choice([i for i, _ in enumerate(scores)])
         action = actions[idx]
@@ -129,12 +141,12 @@ def step():
     global step_ct, agent, Q
     state = get_state()
     action = get_policy_action(state)
-    qval = Q[(state, action)]
+    qval = get_qval(state, action)
 
     # log all action values
     debug('step', step_ct + 1, state, 'action space:')
     for a in get_action_space():
-        debug('\t', a, Q[(state, a)], '[*]' if a == action else '')
+        debug('\t', a, get_qval(state, a), '[*]' if a == action else '')
 
     # take action, make observation
     dx, dy = action_map.get(action)
@@ -148,13 +160,14 @@ def step():
     if not done:
         next_action = get_policy_action(
             observation, use_epsilon=use_sarsa, lookahead=True)
-        next_qval = Q[(observation, next_action)]
+        next_qval = get_qval(observation, next_action)
     else:
         next_qval = 0
 
-    Q[(state, action)] = qval + (learning_rate *
-                                 (reward + ((discount * next_qval) - qval)))
-    debug('\tupdated qval:', Q[(state, action)])
+    new_qval = qval + \
+        (learning_rate * (reward + ((discount * next_qval) - qval)))
+    set_qval(state, action, new_qval)
+    debug('\tupdated qval:', new_qval)
 
     step_ct += 1
     history.append(agent)
@@ -186,16 +199,16 @@ def train(episode_ct):
         rewards=episode_rewards,
         tot_steps=tot_steps,
         tot_random=tot_random,
+        pct_random=(tot_random / tot_steps) * 100,
+        sa_pairs=len(Q),
     )
 
 
 def log_training_stats(stats):
-    print('tot steps', stats['tot_steps'])
-    print('tot random', stats['tot_random'],
-          stats['tot_random'] / stats['tot_steps'])
-    print('Q s/a pair count', len(Q.values()))
+    rewards = stats.pop('rewards')
+    pprint(stats)
 
-    y = np.array(stats['rewards'])
+    y = np.array(rewards)
     y = np.average(y.reshape(-1, 100), axis=1)  # average over n episodes
     x = np.arange(len(y))
 
@@ -207,13 +220,15 @@ def log_training_stats(stats):
 
 
 def play():
-    # temporarily remove randomization for single play and increase logging
+    '''Run single non-random episode'''
+
     global epsilon, verbose
     _old_e = epsilon
     epsilon = 0
     verbose = True
 
     run_episode()
+    log_state()
 
     epsilon = _old_e
     verbose = False
@@ -223,5 +238,4 @@ if __name__ == '__main__':
     episode_ct = int(sys.argv[1]) if len(sys.argv) > 1 else 10000
     stats = train(episode_ct)
     play()
-    log_state()
     log_training_stats(stats)
